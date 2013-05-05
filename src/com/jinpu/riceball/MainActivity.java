a41afc8dd048bcaa020dc17a9118615ff8ec2b89
kangkangma <andy_makang@hotmail.com>
2013-05-06 00:00:44 +0800

    remove unuseful files
    gen, bin, classpath, project

--- src/com/jinpu/riceball/MainActivity.java
+++ src/com/jinpu/riceball/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        super.loadUrl("file:///android_asset/www/index.html");
         super.loadUrl(Config.getStartUrl());
     }
 
